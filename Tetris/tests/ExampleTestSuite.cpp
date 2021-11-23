20ba51729a31f139d9ee1c7e80c63f5b4177e6f2
kyberdrb <kyberdrb@gmail.com>
2021-11-23 14:41:10 +0100

    Transform the ConstructionSite from hard-coded strings to 2D array

--- Tetris/tests/ExampleTestSuite.cpp
+++
@@ -1,8 +0,0 @@
-#include <Brick.h>
-#include "gtest/gtest.h"
-
-class TetrisTestSuite : public ::testing::Test {};
-
-TEST_F(TetrisTestSuite, test_brick_character) {
-    EXPECT_EQ("⎕", Brick().showBrick());
-}
