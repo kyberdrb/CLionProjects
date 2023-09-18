eece14e2e1f320b8608416c325c566090cd82584
kyberdrb <kyberdrb@gmail.com>
2023-09-18 14:49:49 +0200

    Endianness: convert big endian to little endian pt.2

--- Endianness_Little_endian_Big_endian_Conversion/main.cpp
+++ Endianness_Little_endian_Big_endian_Conversion/main.cpp
@@ -41,7 +41,8 @@ int main() {
     number_array_big_endian[2] = (number_raw >> 8) & 0xFF;      // (00010000 00100000 00110000 01000000 >>  8) & 11111111 = 00000000 00010000 00100000 00110000 & 11111111 = 00000000 00000000 00000000 00110000 = 0x30 (2nd byte)
     number_array_big_endian[3] = number_raw & 0xFF;             //  00010000 00100000 00110000 01000000        & 11111111                                                  = 00000000 00000000 00000000 01000000 = 0x40 (3rd byte = LSB)
 
-    std::cout << "reading a number encoded as big endian in a big-endian-CPU-way:    from MSB [0th byte] to LSB [3rd byte] = DECODED AS SAME NUMBER WITH BIG ENDIAN CPU - different with little endian CPU\n";
+    std::cout << "reading a number encoded as big endian in a big-endian-CPU-way:\n"
+                 "from MSB [0th byte] to LSB [3rd byte] = DECODED AS SAME NUMBER WITH BIG ENDIAN CPU - different with little endian CPU\n";
     uint8_t big_endian_number_decoded_as_big_endian[4];
     uint8_t big_endian_number_converted_to_little_endian[4];
     uint8_t destinationByteNumber {3};
@@ -70,29 +71,37 @@ int main() {
 
     std::cout << ":\n";
 
-    std::cout << "reading a number encoded as little endian without conversion in a little-endian-CPU-way: from MSB [3rd byte] to LSB [0th byte] = DECODED AS DIFFERENT NUMBER WITH BIG ENDIAN CPU\n";
+    std::cout << "reading a number encoded as little endian without conversion in a little-endian-CPU-way:\n"
+                 "from MSB [3rd byte] to LSB [0th byte] = DECODED AS DIFFERENT NUMBER WITH BIG ENDIAN CPU\n";
     uint8_t big_endian_number_decoded_as_little_endian[4];
     uint8_t big_endian_number_converted_to_little_endian_2[4];
-    int8_t destinationBytenumber {0};
+    int8_t destination_byte_number {0};
 
-    NumberUnion big_endian_number_decoded_as_little_endian_union[4];
-    NumberUnion big_endian_number_converted_to_little_endian_2_union
+    NumberUnion big_endian_number_decoded_as_little_endian_union;
+    NumberUnion big_endian_number_converted_to_little_endian_2_union;
 
     for (int8_t sourceByteNumber = 3; sourceByteNumber >= 0; --sourceByteNumber) {
         std::cout << std::hex << "0x" << static_cast<uint16_t>(number_array_big_endian[sourceByteNumber]) << "\n";
 
         big_endian_number_decoded_as_little_endian[sourceByteNumber] = number_array_big_endian[sourceByteNumber];
-        big_endian_number_converted_to_little_endian_2[destinationBytenumber] = number_array_big_endian[sourceByteNumber];
+        big_endian_number_converted_to_little_endian_2[destination_byte_number] = number_array_big_endian[sourceByteNumber];
 
+        big_endian_number_decoded_as_little_endian_union.numberArray[sourceByteNumber] = number_array_big_endian[sourceByteNumber];
+        big_endian_number_converted_to_little_endian_2_union.numberArray[destination_byte_number] = number_array_big_endian[sourceByteNumber];
 
-
-        ++destinationBytenumber;
+        ++destination_byte_number;
     }
 
+    std::cout << "\nReconstruct number with reinterpret_cast\n";
     std::cout << std::hex << "0x" << *(reinterpret_cast<uint32_t*>(big_endian_number_decoded_as_little_endian) ) << "\n";
     std::cout << std::hex << "0x" << *(reinterpret_cast<uint32_t*>(big_endian_number_converted_to_little_endian_2) ) << "\n";
+    std::cout << "\nReconstruct number with union\n";
+    std::cout << std::hex << "0x" << big_endian_number_decoded_as_little_endian_union.numberRaw << "\n";
+    std::cout << std::hex << "0x" << big_endian_number_converted_to_little_endian_2_union.numberRaw << "\n";
 
-    // TODO avoiding reinterpret cast with union
+
+
+
 
 //    uint8_t number_array_little_endian[4];
 //    number_array_little_endian[0] = number_raw & 0xFF;
